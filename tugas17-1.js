81fabe8958698bc75b5cba5f0d388ffc27fc6d37
hasanhabsyi2011 <71365610+hasanhabsyi2011@users.noreply.github.com>
2020-09-22 07:46:00 +0700

    Add files via upload

---
+++ tugas17-1.js
@@ -0,0 +1,8 @@
+var kalimat = "Saya beLajar bahaSa peMrograman deNgan khUsuk."
+var kalimat2 = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual."
+
+console.log(kalimat.toLowerCase()) //menjadi lower
+console.log(kalimat.toUpperCase()) //menjadi kapital
+
+console.log(kalimat2.toLowerCase()) //menjadi lower
+console.log(kalimat2.toUpperCase()) //menjadi kapital
